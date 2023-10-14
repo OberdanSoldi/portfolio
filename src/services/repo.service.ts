400f5dc6677eeb40f853d8ff619582c0a78411e5
oberdansoldi <oberdan_soldi@outlook.com.br>
2023-10-13 22:10:56 -0300

    refactor: add env variables

--- src/services/repo.service.ts
+++ src/services/repo.service.ts
@@ -1,9 +1,15 @@
 import type { Repo } from "@/domain/Repo";
 
 class RepoService {
+  private _userName: string;
+
+  constructor() {
+    this._userName = process.env.GITHUB_USER_NAME || "";
+  }
+
   public async getReposData(perPage: number = 5): Promise<Repo[]> {
     const response = await fetch(
-      `https://api.github.com/users/OberdanSoldi/repos?sort=created&type=owner&per_page=${perPage}`
+      `https://api.github.com/users/${this._userName}/repos?sort=created&type=owner&per_page=${perPage}`
     );
     const repos = await response.json();
     return repos;
